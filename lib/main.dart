cdcba593a863ff0a06cd89abb9fad642921f2d8a
HIGH_IN_SKY <103259442+piyushkumar12111111@users.noreply.github.com>
2023-06-12 00:44:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    debugShowCheckedModeBanner: false,
+   // debugShowCheckedModeBanner: false,
   ));
 }
 
